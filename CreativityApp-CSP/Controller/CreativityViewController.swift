de7529dd8f6e821f0894b789595787289a695972
Cody Henrichsen <cody.henrichsen@canyonsdistrict.org>
2019-10-25 08:40:24 -0600

    Initial commit

---
+++ CreativityApp-CSP/Controller/CreativityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreativityApp-CSP
+//
+//  Created by Cody Henrichsen on 10/21/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class CreativityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
