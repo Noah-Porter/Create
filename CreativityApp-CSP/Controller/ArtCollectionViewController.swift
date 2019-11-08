a097901308f134ead26a5643ea40f358235340e4
Porter <npor4690@740s-pl-105.local>
2019-11-08 08:52:17 -0700

    Created the call of the images in the Art Collection file

--- CreativityApp-CSP/Controller/ArtCollectionViewController.swift
+++ CreativityApp-CSP/Controller/ArtCollectionViewController.swift
@@ -10,5 +10,42 @@ import UIKit
 
 class ArtCollectionViewController : UICollectionViewController
 {
+    override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        
+        loadImages()
+    }
     
+    private func loadImages() -> Void
+    {
+        let imageOne : String = "SunGlasses"
+        
+        let imageOneTitle : String = "Colorful sunglasses"
+        
+        let imageTwo : String = "bobpool"
+        
+        let imageTwoTitle : String = "Bob Ross as Dead Pool"
+        
+        let imageThree : String = "Wood"
+        
+        let imageThreeTitle : String = "Broken Wood"
+        
+        let imageFour : String = "Java"
+        
+        let imageFourTitle : String = "Java Haiku"
+        
+        let imageFive : String = "MTM"
+    
+        let imageFiveTitle : String = "MTM Haiku"
+        
+        let imageSix : String = "Swift Haiku"
+    
+        let imageSixTitle : String = "Swift Haiku"
+        
+        let imageList = [imageOne, imageTwo, imageThree, imageFour, imageFive, imageSix]
+        let titleList : [String] = [imageOneTitle, imageTwoTitle, imageThreeTitle, imageFourTitle, imageFiveTitle, imageSixTitle]
+        
+        
+    }
 }
